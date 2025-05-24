c93fff418199c8813b33270bd71ac5122bdc961b
Guilherme da Silva Tesch <tesch.guilherme@hotmail.com>
2025-05-24 18:56:31 -0300

    Primeiro commit dos projetos em Java

---
+++ src/Estudos.java
@@ -0,0 +1,8 @@
+public class Estudos {
+    public static void main(String args []){
+        System.out.println("Anotações de estudo: ");
+        System.out.println(" - Aprender Java");
+        System.out.println(" - Praticar Orientação a Objetos");
+        System.out.println(" - Estudar para CCNA");
+    }
+}
---
+++ src/Perfil.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Perfil {
+    public static void main(String args []){
+        String name = "Guilherme";
+        System.out.println("Olá " + name + ".");
+        System.out.println("Tudo bem?");
+    }
+}
+
+
+
