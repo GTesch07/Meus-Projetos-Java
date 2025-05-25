dba3dcd54ab5e263c1be050208361923eea19052
Guilherme da Silva Tesch <tesch.guilherme@hotmail.com>
2025-05-25 14:39:47 -0300

    Adicionando nova Class While para resolver problema de impressão de numeros 1 a 10

---
+++ src/While.java
@@ -0,0 +1,14 @@
+public class While {
+    public static void main(String args[]){
+        int contador = 1;
+
+        while(contador <= 10){
+            System.out.println(contador);
+            contador ++;
+        }
+        /*for (int i = 1; i <= 10; i ++){
+            System.out.println(contador);
+            contador ++; */
+        }
+    }
+
