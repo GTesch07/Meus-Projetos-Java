6edc76a7afcebc4c55b25bf7bf6de98af8235946
Guilherme da Silva Tesch <tesch.guilherme@hotmail.com>
2025-05-24 21:39:28 -0300

    Adicionando novo Main, testando boas práticas, operadores e formatação de strings

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String args[]){
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+       /* boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;*/
+
+        double media = (9.8 + 6.3 + 8.0) /3;
+        System.out.println(String.format("%.2f", media));
+        System.out.println(media);
+
+        String sinopse1;
+        String sinopse2;
+        System.out.println("");
+        sinopse1 = "Depois de mais de 30 anos de serviço como um dos principais aviadores da Marinha,\n Pete \"Maverick\" Mitchell está de volta, rompendo os limites como um piloto\n de testes corajoso. No mundo contemporâneo das guerras tecnológicas,\n Maverick enfrenta drones e prova que o fator humano ainda é essencial." + "\n" + anoDeLancamento;
+
+        sinopse2 = String.format("""
+                Depois de mais de 30 anos de serviço como um dos principais aviadores da Marinha,
+                 Pete "Maverick" Mitchell está de 
+                 volta,rompendo os limites como um piloto
+                 de testes corajoso. No mundo
+                 contemporâneo das guerras tecnológicas,
+                 Maverick enfrenta drones e prova que fator humano ainda é essencial.
+                 %d
+                """,anoDeLancamento);
+
+        System.out.println(sinopse1 + "\n\n" + sinopse2);
+
+    }
+}
